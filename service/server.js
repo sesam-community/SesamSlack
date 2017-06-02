f4b65ee0d70edc9011e5af53b7b3f92652861149
Eirik Sander-Fjeld <eirik.sander-fjeld@bouvet.no>
2017-06-02 11:34:45 +0200

    Get av user og usergroups returnerer alle elementene, med sesam-properties id, updated og deleted.

--- service/server.js
+++ service/server.js
@@ -3,6 +3,8 @@ var http = require('http');
 var Router = require('node-simple-router');
 var url = require('url');
 
+var token = process.env.slacktoken;
+
 //variables
 var host = "localhost";
 var port = "5000";
@@ -11,20 +13,25 @@ var userlist;
 
 
 function DataAccessLayer() {
-
-
+  var WebClient = require('@slack/client').WebClient;
+  
+  var web = new WebClient(token);
+  
   this.GetUsers = function (callback) {
-    var WebClient = require('@slack/client').WebClient;
-
-    var token = process.env.SLACK_TOKEN || '';
-    var web = new WebClient("xoxp-178195654566-177394975970-189327793635-005d6247cfbe86059052312d9d1c1f9c");
-
     web.users.list(function teamInfoCb(err, info) {
       if (err) {
         console.log('Error:', err);
-      } else {
-        
-        
+      } else {        
+        return callback(info);
+      }
+    });
+  };
+
+  this.GetUsergroups = function (callback) {
+    web.usergroups.list(function teamInfoCb(err, info) {
+      if (err) {
+        console.log('Error:', err);
+      } else {        
         return callback(info);
       }
     });
@@ -36,14 +43,39 @@ var dataAccessLayer = new DataAccessLayer();
 
 var router = Router();
 
-// Configure router to respond with a list of entities to /entities
-router.get("/entities", function (request, response) {
+//henter users fra slack. 
+router.get("/users", function (request, response) {
   var since = url.parse(request.url, true).query.since;
-  console.log(userlist);
   dataAccessLayer.GetUsers(function(userlist) {
-    console.log(userlist);
+    
+
+    Object.keys(userlist.members).forEach(function(element, key, _array) {
+      userlist.members[element]["_deleted"] = userlist.members[element]["deleted"];
+      userlist.members[element]["updated"] = userlist.members[element]["updated"];
+      userlist.members[element]["_id"] = userlist.members[element]["id"];
+    })
     response.writeHead(200, {"Content-Type": "application/json"});
     response.end(JSON.stringify(userlist));
+  })
+});
+
+router.get("/usergroups", function (request, response) {
+  var since = url.parse(request.url, true).query.since;
+  dataAccessLayer.GetUsergroups(function(usergrouplist) {
+      Object.keys(usergrouplist.usergroups).forEach(function(element, key, _array) {
+      var deleted;
+      console.log(usergrouplist.usergroups[element]["deleted_by"])
+      if(usergrouplist.usergroups[element]["deleted_by"]) {
+        deleted = true;
+      } else {
+        deleted = false;
+      }   
+      usergrouplist.usergroups[element]["_deleted"] = deleted;
+      usergrouplist.usergroups[element]["_updated"] = usergrouplist.usergroups[element]["date_update"];
+      usergrouplist.usergroups[element]["_id"] = usergrouplist.usergroups[element]["id"];
+    })
+    response.writeHead(200, {"Content-Type": "application/json"});
+    response.end(JSON.stringify(usergrouplist));
   })
   
 });
