e3ab7a2755cbbdf8cb3cd32fcbd6bcc7076d04a0
Trond <trondtufte89@gmail.com>
2017-10-19 13:41:47 +0200

    refactored users/updateUser

--- service/users.js
+++ service/users.js
@@ -8,20 +8,19 @@ var web = new WebClient(token);
 var exports = module.exports = {};
 
 
-function setProfile(firstName, lastName,userProfile, userId) {
-  var profile={'first_name': firstName,'last_name': lastName};
+function setProfile(firstName, lastName, userProfile, userId) {
+  var profile = { 'first_name': firstName, 'last_name': lastName };
+  //  var profile = userProfile;
+  //  profile["first_name"] = firstName;
+  //  profile[ "last_name"] = lastName;
 
-//   var profile = userProfile;
-//  profile["first_name"] = firstName;
-//  profile[ "last_name"] = lastName;
-
-   var ur = "https://slack.com/api/users.profile.set?token=" + token + "&user="+ userId + "&profile={'first_name':'" + firstName + "','last_name':'"+ lastName + "'}"+"&pretty=1";    
-   var opt = {
+  var ur = "https://slack.com/api/users.profile.set?token=" + token + "&user=" + userId + "&profile={'first_name':'" + firstName + "','last_name':'" + lastName + "'}" + "&pretty=1";
+  var opt = {
     url: ur,
     token: token,
     header: {
       'User-Agent': 'Super Agent/0.0.1',
-      'Content-Type':  'application/x-www-form-urlencoded'
+      'Content-Type': 'application/x-www-form-urlencoded'
     }
   }
   request(opt, function (error, response, body) {
@@ -34,39 +33,37 @@ function setProfile(firstName, lastName,userProfile, userId) {
   });
 }
 
-function setImage(userId,imgUrl) {
-  
-   download(imgUrl, userId + '.png', function () {
-   var img = fs.readFile(userId + '.png', function (err, data) {
-    if (err) {
-      console.log(+"Error downloading file: " + err);
-      return;
-    } else {
-      var ur = "https://slack.com/api/users.setPhoto?token=" + token + "&user="+ userId + "&mage=" + form + "&pretty=1"; 
-      var opt = {
-      url: ur,
-        header: {
-          'User-Agent': 'Super Agent/0.0.1',
-          'Content-Type':  'multipart/form-data'  
+function setImage(userId, imgUrl) {
+
+  download(imgUrl, userId + '.png', function () {
+    var img = fs.readFile(userId + '.png', function (err, data) {
+      if (err) {
+        console.log(+"Error downloading file: " + err);
+        return;
+      } else {
+        var ur = "https://slack.com/api/users.setPhoto?token=" + token + "&user=" + userId + "&mage=" + form + "&pretty=1";
+        var opt = {
+          url: ur,
+          header: {
+            'User-Agent': 'Super Agent/0.0.1',
+            'Content-Type': 'multipart/form-data'
+          }
         }
+        var req = request.post(opt, function (err, response, body) {
+          if (err) {
+            console.log(err);
+          } else {
+            console.log(body);
+          }
+        });
+
+        var form = req.form();
+        form.append('image', fs.createReadStream(userId + ".png"));
+
       }
-      var req = request.post(opt, function (err, response, body) {
-        if (err) {
-          console.log(err);
-        } else {
-          console.log(body);
-        }
-      });
-    
-      var form = req.form();
-      form.append('image', fs.createReadStream(userId + ".png"));
 
-    }
-
+    });
   });
-});
- 
- // form.append('image', fs.createReadStream(imgUrl));
 
 }
 
@@ -77,41 +74,40 @@ var download = function (uri, filename, callback) {
 };
 
 
-exports.updateUser = function(request,response) { 
+exports.updateUser = function (request, response) {
   if (request.method === "POST") {
+    var imgUrl = "";
+    var userId = "";
+    var firstName = "";
+    var lastName = "";
+    var test = "";
+    var profile = "";
+    var userArray = request.body;
+    userArray.forEach(function (element) {
+      profile = element["profile"];
+      userId = element["id"];
+      firstName = element["name"];
+      lastName = element["lastName"];
 
-      var imgUrl = "";
-      var userId = "";
-      var firstName = "";
-      var lastName = "";
-      var test = "";
-      var profile = "";
-      var userArray =   request.body;
-      userArray.forEach(function (element) {
-        profile = element["profile"];
-        userId = element["id"];
-        firstName = element["name"];
-        lastName = element["lastName"];
-
-        if (element["image"] != null) {
-          test = element["image"];
-          if (test["fit_thumb"]["url"] != null) {
-            imgUrl = test["fit_thumb"]["url"];
-          }
+      if (element["image"] != null) {
+        test = element["image"];
+        if (test["fit_thumb"]["url"] != null) {
+          imgUrl = test["fit_thumb"]["url"];
         }
+      }
 
-     console.log("Current token: " + token);
-     setImage(userId, imgUrl);
-     setProfile(firstName, lastName,profile, userId);
-      response.writeHead(200, { "Content-Type": "application/json" });  
+      console.log("Current token: " + token);
+      setImage(userId, imgUrl);
+      setProfile(firstName, lastName, profile, userId);
+      response.writeHead(200, { "Content-Type": "application/json" });
       response.end(JSON.stringify(element));
-      });
+    });
 
   }
 
 }
 
- exports.inviteUser = function(email) {
+exports.inviteUser = function (email) {
   var ur = 'https://slack.com/api/users.admin.invite?token=' + token + '&email=' + email + '&pretty=1';
   var opt = {
     url: ur,
@@ -133,19 +129,19 @@ exports.updateUser = function(request,response) {
 
 function deactivateUser(userId) {
   var ur = 'https://slack.com/api/users.admin.setInactive?token='
-   + token + '&user=' + userId +"&scope=\"identify,read,post,client\"";
+    + token + '&user=' + userId + "&scope=\"identify,read,post,client\"";
   var opt = {
     url: ur,
     header: {
       'User-Agent': 'Super Agent/0.0.1',
       'Content-Type': 'application/x-www-form-urlencoded',
-      
+
     }
   }
-  
+
   request(opt, function (error, response, body) {
     if (!error && response.statusCode == 200) {
-      console.log("Deactivate user: " +userId+ " ok.");
+      console.log("Deactivate user: " + userId + " ok.");
       console.log(response.body);
       return response.statusCode;
     } else {
@@ -155,27 +151,27 @@ function deactivateUser(userId) {
   });
 }
 
-exports.PostUsers = function(req, res) {
-  var userlist = req.body;  
-  
+exports.PostUsers = function (req, res) {
+  var userlist = req.body;
+
   Object(userlist).forEach(function (element, key, _array) {
-    if(element['id'] != "" && element["_deleted"]){
-        deactivateUser(element['id']);
-    }else if (element['id'] != "" && !element["_deleted"]) {
-        //setProfile(element);
-    } else if (element['id'] == "" && !element["_deleted"]){
-        inviteUser(element['email']);
+    if (element['id'] != "" && element["_deleted"]) {
+      deactivateUser(element['id']);
+    } else if (element['id'] != "" && !element["_deleted"]) {
+      //setProfile(element);
+    } else if (element['id'] == "" && !element["_deleted"]) {
+      inviteUser(element['email']);
     }
   });
   res.writeHead(200, { "Content-Type": "application/json" });
   res.end("finished?");
 };
 
-exports.Deactivate = function(req, res) {
-  var userlist = req.body;  
+exports.Deactivate = function (req, res) {
+  var userlist = req.body;
   Object(userlist).forEach(function (element, key, _array) {
-    if(element['id'] != "" && element["_deleted"]){
-      deactivateUser(element['id']);      
+    if (element['id'] != "" && element["_deleted"]) {
+      deactivateUser(element['id']);
     }
   });
   res.writeHead(200, { "Content-Type": "application/json" });
@@ -184,20 +180,18 @@ exports.Deactivate = function(req, res) {
 
 exports.GetUserslist = function (res) {
   GetUsers(function (userlist) {
-      userlist = userlist;
-      Object(userlist.members).forEach(function (element, key, _array) {
-        if (element["id"] == "USLACKBOT" || element["is_bot"] == true) {
-        } else {
-          element["_deleted"] = element["deleted"];
-          element["_updated"] = element["updated"];
-          element["_id"] = element["id"];
-        }
-      });
-      res.writeHead(200, { "Content-Type": "application/json" });
-      res.end(JSON.stringify(userlist));
+    userlist = userlist;
+    Object(userlist.members).forEach(function (element, key, _array) {
+      if (element["id"] == "USLACKBOT" || element["is_bot"] == true) {
+      } else {
+        element["_deleted"] = element["deleted"];
+        element["_updated"] = element["updated"];
+        element["_id"] = element["id"];
+      }
     });
- inviteUser("inger.elise.overa@bouvet.no");
- console.log("Invited!");
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify(userlist));
+  });
 };
 
 GetUsers = function (callback) {
